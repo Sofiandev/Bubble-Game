de492f10e5d1b5081f0ad82efc861bba45a4268d
SofianAcDev <sofian.achbabi@gmail.com>
2023-02-26 16:39:18 +0100

    Bubble Game - js

---
+++ app.js
@@ -0,0 +1,31 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  const size = Math.random() * 200 + 100;
+
+  bubble.classList.add("bubble");
+
+  document.body.appendChild(bubble);
+
+  bubble.style.height = size + "px";
+  bubble.style.width = size + "px";
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    console.log(counter);
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 1000);
